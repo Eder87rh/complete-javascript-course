d462996f830de8526bbe13c9d406f085b585defb
Eder Ramirez <eder.ramirez87@gmail.com>
2019-06-19 16:39:48 -0500

    Adding new item to the UI

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -77,7 +77,7 @@ interviewQuestion2('estadisticas')('naye');*/
  *Bind call and apply
  */
 
- var john = {
+ /*var john = {
    name: 'John',
    age: 26,
    job: 'teacher',
@@ -112,4 +112,80 @@ interviewQuestion2('estadisticas')('naye');*/
  johnFriendly('morning');
 
  var emilyFormal = john.presentation.bind(emily, 'formal');
- emilyFormal('evening');
+ emilyFormal('evening');*/
+
+/*var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr ,fn){
+  var arrRes = [];
+  for(var i= 0; i < arr.length; i++){
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+  return el >= limit;
+}
+
+ var ages = arrayCalc(years, calculateAge);
+ var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+ console.log(ages)
+ console.log(fullJapan)*/
+
+(function() {
+  var Question = function(question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers
+    this.correctAnswer = correctAnswer
+  };
+
+  Question.prototype.displayQuestion = function(){
+    console.log(this.question);
+
+    for(i in this.answers){
+      console.log(i, ": " + this.answers[i]);
+    }
+  };
+
+  Question.prototype.showResult = function(answer) {
+
+    if(Number(answer) === this.correctAnswer) {
+      console.log("Correct!!")
+      score++; 
+    } else if (answer === "exit") {
+      console.log(`Your score is: ${score}`)
+      return;
+    } else {
+      console.log("incorrect :(");
+    }  
+
+    console.log(`Your score is: ${score}`)
+    doQuestion();
+
+  };
+
+  doQuestion = function() {
+    var randomIndex = Math.round(Math.random() * 2);
+
+    var questionSelected = arrQuestions[randomIndex];
+    questionSelected.displayQuestion();
+
+    var res =  prompt("Please select the correct answer (just type the number)");
+    questionSelected.showResult(res);
+  }
+
+  var question1 = new Question('¿Quien descubrió América?', ["josé josé", "pepe el toro", "Cristobal Colón"], 2);
+  var question2 = new Question('¿Cuanto es 2 + 2?', [4, 5, -1], 0);
+  var question3 = new Question('¿Cuanto es 5 * 5?', [25, 3, -31], 0);
+
+  var arrQuestions = [ question1, question2, question3 ];
+  var score = 0;
+
+  doQuestion();
+})()
+
